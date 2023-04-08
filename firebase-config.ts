e493cbe7821a4fa252eb04767938880edc296a2a
kuzeykose <kosekuzey97@gmail.com>
2023-04-08 14:49:12 -0400

    Hotfix

--- firebase-config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyDoIBUg4IaEiibvhwgNMCGh0npHqD-0TVg',
-  authDomain: 'my-blog-d3a4b.firebaseapp.com',
-  projectId: 'my-blog-d3a4b',
-  storageBucket: 'my-blog-d3a4b.appspot.com',
-  messagingSenderId: '1067378855348',
-  appId: '1:1067378855348:web:1f727b51cd7a36f4ed76e1',
-  measurementId: 'G-F3X602HZBJ',
-};
--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../../../../firebase';
+import { db } from '@/firebase';
 
 export async function GET() {
   const blogsRef = collection(db, 'blogs');
