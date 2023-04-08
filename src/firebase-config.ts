6239e7be3ca1cde247357c28313f5a96590b8c31
kuzeykose <kosekuzey97@gmail.com>
2023-04-08 15:47:12 -0400

    Hotfix

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: process.env.API_KEY, 
+  apiKey: process.env.API_KEY,
   authDomain: 'my-blog-d3a4b.firebaseapp.com',
   projectId: 'my-blog-d3a4b',
   storageBucket: 'my-blog-d3a4b.appspot.com',
   messagingSenderId: '1067378855348',
-  appId: process.env.APP_ID, '1:1067378855348:web:1f727b51cd7a36f4ed76e1',
+  appId: process.env.APP_ID,
   measurementId: 'G-F3X602HZBJ',
 };
