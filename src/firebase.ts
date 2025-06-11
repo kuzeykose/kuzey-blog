7a50f7b721b2d8d778a5ee5a67cdcf0969ab158f
Kuzey Kose <kosekuzey97@gmail.com>
2025-06-11 12:21:25 -0400

    vercel/next example blog

--- src/firebase.ts
+++
@@ -1,7 +0,0 @@
-import { firebaseConfig } from './firebase-config';
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
